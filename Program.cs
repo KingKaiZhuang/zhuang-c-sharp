0d41e3039481e69a423c24708df9e1986112457a
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-10 11:38:54 +0800

    change

--- Program.cs
+++ Program.cs
@@ -15,5 +15,16 @@ class Program
         {
             Console.WriteLine("不及格！");
         }
+
+        // 正確的初始化Person物件
+        Person person1 = new Person();
+        person1.Name = "Alice";
+        person1.Age = 25;
     }
+}
+
+class Person
+{
+    public string Name;
+    public int Age;
 }
