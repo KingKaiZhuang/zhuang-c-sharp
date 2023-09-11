d85682cbddf8ca92dd7d0a1658c40834b292cef4
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-11 14:17:46 +0800

    OrderItem

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
-using Animal;
+using System.Collections.Generic;
 
-Person personOne = new Person();
-personOne.name = "zhuang";
-personOne.age = 17;
-personOne.height = 170;
-personOne.Sayhi();
-Console.WriteLine(personOne.adult());
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 創建一個 List 來儲存飲料
+            List<Drink> drinks = new List<Drink>();
 
-Console.WriteLine(personOne.name);
-Console.WriteLine(personOne.age);
-Console.WriteLine(personOne.height);
+            AddNewDrink(drinks);
 
+            // 顯示每種飲料的資訊
+            DisPlayDrinkMenu(drinks);
+        }
 
+        private static void DisPlayDrinkMenu(List<Drink> drinks)
+        {
+            Console.WriteLine("品名     容量     價格");
+            foreach (var drink in drinks)
+            {
+                Console.WriteLine($"飲料名稱: {drink.Name,-5} 尺寸: {drink.Size,-5} 價格: {drink.Price,-5}");
+            }
+        }
 
+        private static void AddNewDrink(List<Drink> drinks)
+        {
+            drinks.Add(new Drink() { Name = "紅茶", Size = "大杯", Price = 50 });
+            drinks.Add(new Drink() { Name = "綠茶", Size = "中杯", Price = 45 });
+            drinks.Add(new Drink() { Name = "咖啡", Size = "中杯", Price = 60 });
+            drinks.Add(new Drink() { Name = "柳橙汁", Size = "小杯", Price = 40 });
+            drinks.Add(new Drink() { Name = "可樂", Size = "大杯", Price = 55 });
+        }
+    }
+
+}
---
+++ drink.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    // Drink類別代表一種飲料
+    internal class Drink
+    {
+        // 飲料的名稱
+        public String Name { get; set; }
+
+        // 飲料的尺寸
+        public String Size { get; set; }
+
+        // 飲料的價格
+        public int Price { get; set; }
+    }
+}
