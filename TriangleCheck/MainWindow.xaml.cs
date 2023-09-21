90bf3e9ff4839c6aa21494cd2a66619dfc697fc6
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-21 11:32:52 +0800

    update

--- TriangleCheck/MainWindow.xaml.cs
+++ TriangleCheck/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace TriangleCheck
             
         }
 
+
         private void buttonClick(object sender, RoutedEventArgs e)
         {
             isTriangle();
@@ -42,28 +43,29 @@ namespace TriangleCheck
             bool success1 = double.TryParse(triangleInput1.Text, out number1);
             bool success2 = double.TryParse(triangleInput2.Text, out number2);
             bool success3 = double.TryParse(triangleInput3.Text, out number3);
+            outputBlock.Text = "";
             if (success1 && success2 && success3)
             {
                 if (number1 > 0 && number2 > 0 && number3 > 0 && number1 + number2 > number3 && number1 + number2 > number3 && number1 + number2 > number3)
                 {
-                    Triangle triangle = new Triangle(number1,number2,number3);
-                    triangles.Add(triangle);
-                    outputBlock.Text += $"{number1}、{number2}、{number3} 可以形成三角形!\n";
-                    triangles.Add(new Triangle { side1 = number1, side2 = number2, side3 = number3 });
+                    triangles.Add(new Triangle { side1 = number1, side2 = number2, side3 = number3, message = "可以形成三角形!" });
                     resultOutput.Background = Brushes.Green;
-                    resultOutput.Text = $"{number1}、{number2}、{number3} 可以形成三角形!\n";
+                    resultOutput.Text = $"{number1}、{number2}、{number3} 可以形成三角形!";
                 }
                 else
                 {
-                    outputBlock.Text += $"{number1}、{number2}、{number3} 無法形成三角形!\n";
-                    triangles.Add(new Triangle { side1 = number1, side2 = number2, side3 = number3 });
+                    triangles.Add(new Triangle { side1 = number1, side2 = number2, side3 = number3 ,message = "無法形成三角形!" });
                     resultOutput.Background = Brushes.Red;
-                    resultOutput.Text = $"{number1}、{number2}、{number3} 無法形成三角形!\n";
+                    resultOutput.Text = $"{number1}、{number2}、{number3} 無法形成三角形!";
                 }
             }
             else
             {
                 MessageBox.Show("請輸入大於0的數字哦!");
+            }
+            foreach (Triangle triangle in triangles)
+            {
+                outputBlock.Text += $"{triangle.side1}、{triangle.side2}、{triangle.side3}，{triangle.message}\n";
             }
         }
     }
--- TriangleCheck/Triangle.cs
+++ TriangleCheck/Triangle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TriangleCheck
 {
@@ -11,5 +12,6 @@ namespace TriangleCheck
         public double side1 {  get; set; }
         public double side2 { get; set; }
         public double side3 { get; set; }
+        public string message {  get; set; }
     }
 }
