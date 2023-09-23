3b27beb3f59e2e1369a1041315580a814962d744
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-23 13:53:06 +0800

    update

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -20,68 +20,27 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<int> primes = new List<int>();
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Button1_Click(object sender, RoutedEventArgs e)
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int number;
-            bool success = int.TryParse(Text_item.Text, out number);
+            TextBox targetTextBox = sender as TextBox;
+            bool success = int.TryParse(targetTextBox.Text, out int amount);
+
             if(!success)
             {
-                MessageBox.Show("請輸入整數!");
-            }
-            else if(number <= 2)
+                MessageBox.Show("請輸入整數", "輸入錯誤");
+            }else if(amount <= 0)
             {
-                MessageBox.Show($"輸入數值為{number}，小於2，請重新輸入", "輸入錯誤");
+                MessageBox.Show("請輸入正整數", "輸入錯誤");
             }
             else
             {
-                MessageBox.Show($"輸入數值為{number}，以下是他的質數跟倍數~");
-                for(int i = 2;i < number;i++)
-                {
-                    if (isPrime(i))
-                    {
-                        primes.Add(i);
-                    }
-                }
+
             }
-            ListPrimePrint(primes, number);
         }
-
-        private void ListPrimePrint(List<int> primes, int number)
-        {
-            string primeList = $"小於{number}的質數 : ";
-            string primeMultiple = "";
-            foreach(int p in primes)
-            {
-                primeList += $"{p}";
-                primeMultiple += $"{p}的倍數為:";
-                int i = 1;
-                while(p*i <= number)
-                {
-                    primeMultiple += $"{p * i}";
-                    i++;
-                }
-                primeMultiple += "\n";
-            }
-            textblock_prime.Text = primeList;
-            textBlock_multiple.Text = primeMultiple;
-
-
-        }
-
-        private bool isPrime(int p)
-        {
-            // 判斷p是否為質數
-            for (int i = 2; i < p; i++)
-            {
-                if (p % i == 0) return false;
-            }
-            return true;
-        }
-
     }
 }
