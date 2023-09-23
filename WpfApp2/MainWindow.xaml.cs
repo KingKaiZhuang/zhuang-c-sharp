69f155da2b2931f4475b411005435af8b5b4f96b
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-09-23 15:22:39 +0800

    update

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         Dictionary<string,int> drinks = new Dictionary<string,int>();
@@ -36,7 +33,6 @@ namespace WpfApp2
             mydrinks.Add("綠茶小杯", 60);
             mydrinks.Add("咖啡大杯", 60);
             mydrinks.Add("咖啡小杯", 60);
-
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -64,40 +60,39 @@ namespace WpfApp2
         {
             double total = 0.0;
             double sellPrice = 0.0;
-            string displayString = "";
+            string displayString = "訂購清單如下 : \n";
+            string message = "";
 
             foreach (KeyValuePair<string, int> item in orders)
             {
                 string drinkName = item.Key;
                 int amount = orders[drinkName];
                 int price = drinks[drinkName];
-                string message = "訂購清單如下 : \n";
                 total += price * amount;
-                displayString += $"{drinkName} X {amount}杯，每杯{price}元，總共{price}元";
-
-                if (total >= 500)
-                {
-                    message = "訂單滿500元以上者8折";
-                    sellPrice = total * 0.8;
-                }
-                else if (total >= 300)
-                {
-                    message = "訂單滿300元以上者85折";
-                    sellPrice = total * 0.85;
-                }
-                else if (total >= 200)
-                {
-                    message = "訂單滿200元以上者9折";
-                    sellPrice = total * 0.9;
-                }
-                else
-                {
-                    message = "訂單未滿200元不打折";
-                    sellPrice = total;
-                }
-                displayString += $"本次訂購總共{orders.Count}項，{message}，總共{sellPrice}元!";
-                TextBlock1.Text = displayString;
+                displayString += $"{drinkName} X {amount}杯，每杯{price}元，總共{price * amount}元\n";
             }
+            if (total >= 500)
+            {
+                message = "訂單滿500元以上者8折";
+                sellPrice = total * 0.8;
+            }
+            else if (total >= 300)
+            {
+                message = "訂單滿300元以上者85折";
+                sellPrice = total * 0.85;
+            }
+            else if (total >= 200)
+            {
+                message = "訂單滿200元以上者9折";
+                sellPrice = total * 0.9;
+            }
+            else
+            {
+                message = "訂單未滿200元不打折";
+                sellPrice = total;
+            }
+            displayString += $"本次訂購總共{orders.Count}項，{message}，總共{sellPrice}元!\n";
+            TextBlock1.Text = displayString;
         }
     }
 }
