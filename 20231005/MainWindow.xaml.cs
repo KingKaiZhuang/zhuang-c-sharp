453dc20a0d0960791d91696a16590817e9f8ccc1
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-04 01:17:45 +0800

    update

---
+++ 20231005/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿// 解題步驟
+// 將飲料清單匯入
+// 顯示飲料清單
+// 按下按鈕 將訂購的飲料加入清單 顯示訂單內容
+using System.Windows;
+
+namespace _20231005
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
