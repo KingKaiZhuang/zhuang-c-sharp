c1dce77417315a57e5c3c74a86032ac40ccd8706
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-10 23:16:56 +0800

    update

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string takeout = "";
+        Dictionary<string,int> drinks = new Dictionary<string,int>();
+        Dictionary<string,int> orders = new Dictionary<string,int>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 新增飲料品項
+            addDrink(drinks);
+            // 顯示飲料清單
+            showDrink(drinks);
+        }
+
+        private void showDrink(Dictionary<string, int> myDrinks)
+        {
+            foreach (var drink in myDrinks)
+            {
+                var sp = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal
+                };
+
+                var cb = new CheckBox
+                {
+                    Content = $"{drink.Key} : {drink.Value}元",
+                    Width = 200,
+                    FontFamily = new FontFamily("Consolas"),
+                    FontSize = 18,
+                    Foreground = Brushes.Blue,
+                    Margin = new Thickness(5)
+                };
+
+                var sl = new Slider
+                {
+                    Width = 100,
+                    Value = 0,
+                    Minimum = 0,
+                    Maximum = 10,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    IsSnapToTickEnabled = true
+                };
+
+                var lb = new Label
+                {
+                    Width = 50,
+                    Content = "0",
+                    FontFamily = new FontFamily("Consolas"),
+                    FontSize = 18,
+                    Foreground = Brushes.Red
+                };
+
+                sp.Children.Add(cb);
+                sp.Children.Add(sl);
+                sp.Children.Add(lb);
+
+                //資料繫結
+                Binding myBinding = new Binding("Value");
+                myBinding.Source = sl;
+                lb.SetBinding(ContentProperty, myBinding);
+
+                menuShow.Children.Add(sp);
+            }
+        }
+
+        private void addDrink(Dictionary<string, int> myDrinks)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV檔案|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+                //string alltext = File.ReadAllText(filename);
+                string[] lines = File.ReadAllLines(filename);
+                foreach (var line in lines)
+                {
+                    string[] tokens = line.Split(',');
+                    string drinkName = tokens[0];
+                    int price = Convert.ToInt32(tokens[1]);
+                    myDrinks.Add(drinkName, price);
+                }
+            }
+
+        }
+
+        private void check_box(object sender, RoutedEventArgs e)
+        {
+            var rb = sender as RadioButton;
+            if (rb.IsChecked == true) takeout = rb.Content.ToString();
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            // 顯示訂購結果
+            displayOrder();
+        }
+
+        private void displayOrder()
+        {
+            orderResult.Inlines.Clear();
+            Run titleString = new Run
+            {
+                Text = "您所訂購的訂單為",
+                FontSize = 16,
+                FontWeight = FontWeights.Bold,
+                Foreground = Brushes.Blue,
+            };
+
+            Run takeoutString = new Run
+            {
+                Text = $"{takeout}:",
+                FontSize = 16,
+                FontWeight = FontWeights.Bold,
+                Foreground = Brushes.Red,
+            };
+
+            orderResult.Inlines.Add(titleString);
+            orderResult.Inlines.Add(takeoutString);
+        }
+    }
+}
