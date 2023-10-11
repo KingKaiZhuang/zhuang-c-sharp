2b3a8b2d7d745e118b8d2f5de916d7d755969b79
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-11 10:00:27 +0800

    update

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,25 +1,15 @@
-﻿using Microsoft.Win32;
+﻿ using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         string takeout = "";
@@ -27,6 +17,9 @@ namespace WpfApp3
         Dictionary<string,int> orders = new Dictionary<string,int>();
         public MainWindow()
         {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
             InitializeComponent();
             // 新增飲料品項
             addDrink(drinks);
@@ -88,7 +81,7 @@ namespace WpfApp3
         private void addDrink(Dictionary<string, int> myDrinks)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "CSV檔案|*.csv";
+            openFileDialog.Filter = "CSV檔案|*.csv|文字檔案|*.txt|全部檔案|*.*";
             if (openFileDialog.ShowDialog() == true)
             {
                 string filename = openFileDialog.FileName;
@@ -113,11 +106,31 @@ namespace WpfApp3
 
         private void button_click(object sender, RoutedEventArgs e)
         {
+            // 將訂購的飲料加入訂單
+            placeOrder(orders);
             // 顯示訂購結果
-            displayOrder();
+            displayOrder(orders);
         }
 
-        private void displayOrder()
+        private void placeOrder(Dictionary<string, int> myOrders)
+        {
+            myOrders.Clear();
+            for(int i = 0;i < menuShow.Children.Count;i++)
+            {
+                var sp = menuShow.Children[i] as StackPanel;
+                var cb = sp.Children[0] as CheckBox;
+                var sl = sp.Children[1] as Slider;
+                string drinkName = cb.Content.ToString().Substring(0,4);
+                int quantity = Convert.ToInt32(sl.Value);
+
+                if (cb.IsChecked == true && quantity != 0)
+                {
+                    myOrders.Add(drinkName, quantity);
+                }
+            }
+        }
+
+        private void displayOrder(Dictionary<string, int> myOrders)
         {
             orderResult.Inlines.Clear();
             Run titleString = new Run
@@ -138,6 +151,53 @@ namespace WpfApp3
 
             orderResult.Inlines.Add(titleString);
             orderResult.Inlines.Add(takeoutString);
+            orderResult.Inlines.Add(new Run() { Text = " ，訂購明細如下: \n", FontSize = 16 });
+
+            double total = 0.0;
+            double sellPrice = 0.0;
+            //string displayString = $"本次訂購為{takeout}，清單如下：\n";
+            string discountString = "";
+
+            int i = 1;
+            foreach (var item in myOrders)
+            {
+                string drinkName = item.Key;
+                int quantity = myOrders[drinkName];
+                int price = drinks[drinkName];
+                total += price * quantity;
+                orderResult.Inlines.Add(new Run() { Text = $"飲料品項{i}： {drinkName} X {quantity}杯，每杯{price}元，總共{price * quantity}元\n" });
+                i++;
+            }
+
+            if (total >= 500)
+            {
+                discountString = "訂購滿500元以上者打8折";
+                sellPrice = total * 0.8;
+            }
+            else if (total >= 300)
+            {
+                discountString = "訂購滿300元以上者打85折";
+                sellPrice = total * 0.85;
+            }
+            else if (total >= 200)
+            {
+                discountString = "訂購滿200元以上者打9折";
+                sellPrice = total * 0.9;
+            }
+            else
+            {
+                discountString = "訂購未滿200元以上者不打折";
+                sellPrice = total;
+            }
+
+            Italic summaryString = new Italic(new Run
+            {
+                Text = $"本次訂購總共{myOrders.Count}項，{discountString}，售價{sellPrice}元",
+                FontSize = 16,
+                FontWeight = FontWeights.Bold,
+                Foreground = Brushes.Red
+            });
+            orderResult.Inlines.Add(summaryString);
         }
     }
 }
