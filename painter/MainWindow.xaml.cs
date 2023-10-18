4bc9a8e2823efa1431ea6aa50be4b3c020272741
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-18 12:19:07 +0800

    update

--- painter/MainWindow.xaml.cs
+++ painter/MainWindow.xaml.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _2023_WpfApp3
+namespace painter
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        String shapeType = "Line";
+        string shape;
         Color strokeColor = Colors.Red;
-        int strokeThickness = 1;
+        int strukeThickness;
+        Brush strukeBrush = new SolidColorBrush(Colors.Red);
         Point start, dest;
         public MainWindow()
         {
@@ -23,95 +31,31 @@ namespace _2023_WpfApp3
             strokeColorPicker.SelectedColor = strokeColor;
         }
 
-        private void ShapeButton_Click(object sender, RoutedEventArgs e)
+        private void shape_click(object sender, RoutedEventArgs e)
         {
-            var targetRadioButton = sender as RadioButton;
-            shapeType = targetRadioButton.Tag.ToString();
-            //MessageBox.Show(shapeType);
+            var shapeChoose = sender as RadioButton;
+            shape = shapeChoose.Tag.ToString();
+            MessageBox.Show(shape);
         }
 
-        private void strokeColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        private void colorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            strokeColor = (Color)strokeColorPicker.SelectedColor;
-            //MessageBox.Show(strokeColor.ToString());
+            strukeThickness = Convert.ToInt32(colorSlider.Value);
         }
 
-        private void thicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            strokeThickness = Convert.ToInt32(thicknessSlider.Value);
-        }
-
-        private void myCanvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            myCanvas.Cursor = Cursors.Cross;
             start = e.GetPosition(myCanvas);
-            DisplayStatus();
-
-            switch (shapeType)
+            myCanvas.Cursor = Cursors.Cross;
+            switch(shape)
             {
                 case "Line":
-                    Brush stroke = new SolidColorBrush(Colors.Gray);
-                    Line line = new Line
-                    {
-                        X1 = start.X,
-                        Y1 = start.Y,
-                        X2 = dest.X,
-                        Y2 = dest.Y,
-                        Stroke = stroke,
-                        StrokeThickness = 1,
-                    };
-                    myCanvas.Children.Add(line);
                     break;
-                case "Rectangle":
+                case "rectangle":
                     break;
-                case "Ellipse":
+                case "circle":
                     break;
             }
-        }
-
-        private void myCanvas_MouseMove(object sender, MouseEventArgs e)
-        {
-            dest = e.GetPosition(myCanvas);
-            DisplayStatus();
-
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                switch (shapeType)
-                {
-                    case "Line":
-                        var line = myCanvas.Children.OfType<Line>().LastOrDefault();
-                        line.X2 = dest.X;
-                        line.Y2 = dest.Y;
-                        break;
-                    case "Rectangle":
-                        break;
-                    case "Ellipse":
-                        break;
-                }
-            }
-        }
-
-        private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            switch (shapeType)
-            {
-                case "Line":
-                    var line = myCanvas.Children.OfType<Line>().LastOrDefault();
-                    Brush stroke = new SolidColorBrush(strokeColor);
-                    line.Stroke = stroke;
-                    line.StrokeThickness = strokeThickness;
-                    break;
-                case "Rectangle":
-                    break;
-                case "Ellipse":
-                    break;
-            }
-        }
-
-        private void DisplayStatus()
-        {
-            coordinateLabel.Content = $"座標點：({Math.Round(start.X)}, {Math.Round(start.Y)}) - ({Math.Round(dest.X)}, {Math.Round(dest.Y)})";
-            shapeLabel.Content = $"Line: {myCanvas.Children.OfType<Line>().Count<Line>()}";
         }
     }
 }
