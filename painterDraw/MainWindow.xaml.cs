81c49ac4654d028e11352d0bb08ad99fd9e1c227
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-24 15:01:44 +0800

    painterDraw-1

--- painterDraw/MainWindow.xaml.cs
+++ painterDraw/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace painterDraw
     /// </summary>
     public partial class MainWindow : Window
     {
-        string shapeType = "";
+        string shapeType = "Line";
         int strokeThickness = 1;
         Color strokeColor = Colors.Red;
         Point start, dest;
@@ -43,6 +43,26 @@ namespace painterDraw
             strokeThickness = Convert.ToInt32(strokeThicknessSlider.Value);
         }
 
+        private void myCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            dest = e.GetPosition(myCanvas);
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                switch (shapeType)
+                {
+                    case "Line":
+                        var line = myCanvas.Children.OfType<Line>().LastOrDefault();
+                        line.X2 = dest.X;
+                        line.Y2 = dest.Y;
+                        break;
+                    case "Rectangle":
+                        break;
+                    case "Ellipse":
+                        break;
+                }
+            }
+        }
+
         private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             start = e.GetPosition(myCanvas);
@@ -50,12 +70,38 @@ namespace painterDraw
             switch (shapeType)
             {
                 case "Line":
+                    DrawLine(Colors.Gray, 1);
                     break;
                 case "Rectangle":
                     break;
                 case "Ellipse":
                     break;
             }
+            DisplayStatus();
+        }
+
+        private void DisplayStatus()
+        {
+            coordinateLabel.Content = $"座標點:({Math.Round(start.X)} , {Math.Round(start.Y)}) : ({Math.Round(dest.X)} , {Math.Round(dest.Y)})";
+        }
+
+        private void myCanvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DrawLine(Color color, int thickness)
+        {
+            Brush stroke = new SolidColorBrush(color);
+            Line line = new Line
+            {
+                Stroke = stroke,
+                X1 = start.X,
+                Y1 = start.Y,
+                X2 = dest.X,
+                Y2 = dest.Y
+            };
+            myCanvas.Children.Add(line);
         }
     }
 }
