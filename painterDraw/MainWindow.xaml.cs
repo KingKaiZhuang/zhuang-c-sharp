32b03276251f86ee37bc1cce8b86a680da28fa9f
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-25 10:32:21 +0800

    painterDraw-3

--- painterDraw/MainWindow.xaml.cs
+++ painterDraw/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -54,8 +53,30 @@ namespace painterDraw
                         line.Y2 = dest.Y;
                         break;
                     case "Rectangle":
+                        Point origin2 = new Point
+                        {
+                            X = Math.Min(start.X, dest.X),
+                            Y = Math.Min(start.Y, dest.Y)
+                        };
+
+                        var rect = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                        rect.SetValue(Canvas.LeftProperty, origin2.X);
+                        rect.SetValue (Canvas.TopProperty, origin2.Y);
+                        rect.Width = Math.Abs(dest.X - start.X);
+                        rect.Height = Math.Abs(dest.Y - start.Y);
                         break;
                     case "Ellipse":
+                        Point origin3 = new Point
+                        {
+                            X = Math.Min(start.X, dest.X),
+                            Y = Math.Min(start.Y, dest.Y)
+                        };
+
+                        var ellipse = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                        ellipse.SetValue(Canvas.LeftProperty,origin3.X);
+                        ellipse.SetValue(Canvas.TopProperty, origin3.Y);
+                        ellipse.Width = Math.Abs(dest.X - start.X);
+                        ellipse.Height = Math.Abs(dest.Y - start.Y);
                         break;
                 }
             }
@@ -75,15 +96,22 @@ namespace painterDraw
                     {
                         Stroke = Brushes.Gray,
                         StrokeThickness = 1,
-                        Fill = Brushes.LightGray,
-                        Width = 30,
-                        Height = 50
+                        Fill = Brushes.LightGray
                     };
                     myCanvas.Children.Add(rect);
                     rect.SetValue(Canvas.LeftProperty, start.X);
                     rect.SetValue(Canvas.TopProperty, start.Y);
                     break;
                 case "Ellipse":
+                    var ellipse = new Ellipse
+                    {
+                        Stroke = Brushes.Gray,
+                        StrokeThickness = 1,
+                        Fill = Brushes.LightGray
+                    };
+                    myCanvas.Children.Add(ellipse);
+                    ellipse.SetValue(Canvas.LeftProperty, start.X);
+                    ellipse.SetValue (Canvas.TopProperty, start.Y);
                     break;
             }
             DisplayStatus();
@@ -94,13 +122,19 @@ namespace painterDraw
         {
             int LineCount = myCanvas.Children.OfType<Line>().Count();
             int rectCount = myCanvas.Children.OfType<Rectangle>().Count();
+            int ElliCount = myCanvas.Children.OfType<Ellipse>().Count();
             coordinateLabel.Content = $"座標點:({Math.Round(start.X)} , {Math.Round(start.Y)}) : ({Math.Round(dest.X)} , {Math.Round(dest.Y)})";
-            shapeLabel.Content = $"Line : {LineCount} Rectangle : {rectCount}";
+            shapeLabel.Content = $"Line : {LineCount} Rectangle : {rectCount} Ellipse : {ElliCount}";
         }
 
         private void strokeColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
             strokeColor = (Color)strokeColorPicker.SelectedColor;
+        }
+
+        private void fillColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fillColor = (Color)fillColorPicker.SelectedColor;
         }
 
         private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -113,10 +147,24 @@ namespace painterDraw
                     line.StrokeThickness = strokeThickness;
                     break;
                 case "Rectangle":
+                    var rect = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                    rect.Stroke = new SolidColorBrush(strokeColor);
+                    rect.Fill = new SolidColorBrush(fillColor);
+                    rect.StrokeThickness = strokeThickness;
                     break;
                 case "Ellipse":
+                    var elli = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                    elli.Stroke = new SolidColorBrush(strokeColor);
+                    elli.StrokeThickness = strokeThickness;
+                    elli.Fill = new SolidColorBrush(fillColor);
                     break;
             }
+            myCanvas.Cursor = Cursors.Arrow;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myCanvas.Children.Clear();
         }
 
         private void DrawLine(Color color, int thickness)
