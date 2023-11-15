6bb45cda911309a7467a9cb8d2705005c6d88c16
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-11-15 10:50:59 +0800

    update

--- wordCopy/MainWindow.xaml.cs
+++ wordCopy/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using wordCopy;
 
 namespace wordCopy
 {
@@ -28,7 +29,7 @@ namespace wordCopy
         private void openButton_Click(object sender, RoutedEventArgs e)
         {
             MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
-            myDocumentViewer.ShowDialog();
+            myDocumentViewer.Show();
         }
     }
 }
--- wordCopy/MyDocumentViewer.xaml.cs
+++ wordCopy/MyDocumentViewer.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace wordCopy
 {
@@ -10,7 +12,38 @@ namespace wordCopy
         public MyDocumentViewer()
         {
             InitializeComponent();
-            
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            // 在這裡實現「新建」的操作，例如打開一個新文件、清空文檔等
+            MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
+            myDocumentViewer.Show();
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("OpenCommand");
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("SaveCommand");
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            //判斷選中的文字是否為粗體，並同步更新boldButton的狀態
+            object property = rtbEditor.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            boldButton.IsChecked = (property is FontWeight && (FontWeight)property == FontWeights.Bold);
+
+            //判斷選中的文字是否為斜體，並同步更新italicButton的狀態
+            property = rtbEditor.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            italicButton.IsChecked = (property is FontStyle && (FontStyle)property == FontStyles.Italic);
+
+            //判斷選中的文字是否有底線，並同步更新underlineButton的狀態
+            property = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineButton.IsChecked = (property != DependencyProperty.UnsetValue && property == TextDecorations.Underline);
         }
     }
 }
