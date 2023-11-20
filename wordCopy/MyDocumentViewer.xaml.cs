75c74dc81d4615fbf0e4bed41f47be0994affd1f
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-11-20 14:45:11 +0800

    update

--- wordCopy/MyDocumentViewer.xaml.cs
+++ wordCopy/MyDocumentViewer.xaml.cs
@@ -21,9 +21,10 @@ namespace wordCopy
             {
                 fontFamilyComboBox.Items.Add(fontFamily.Source);
             }
-            fontFamilyComboBox.SelectedIndex = 8;
+            fontFamilyComboBox.SelectedIndex = 2;
 
             fontSizeComboBox.ItemsSource = new List<Double>() { 8,9,10,12,20,24,32,40,50,60,80,90};
+            fontSizeComboBox.SelectedIndex = 3;
         }
 
         private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -57,6 +58,44 @@ namespace wordCopy
             //判斷選中的文字是否有底線，並同步更新underlineButton的狀態
             property = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
             underlineButton.IsChecked = (property != DependencyProperty.UnsetValue && property == TextDecorations.Underline);
+
+            // 判斷選中的文字的字體，同步更新fon
+            property = rtbEditor.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            fontFamilyComboBox.SelectedItem = property;
+            // 判斷選中的文字的字體大小
+            property = rtbEditor.Selection.GetPropertyValue(TextElement.ForegroundProperty) as SolidColorBrush;
+            fontSizeComboBox.SelectedItem = property;
+            
+            SolidColorBrush? foregroundPropety = rtbEditor.Selection.GetPropertyValue(TextElement.ForegroundProperty) as SolidColorBrush;
+            
+            if(foregroundPropety != null)
+            {
+                fontColorPicker.SelectedColor = foregroundPropety.Color;
+            }
         }
+
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontColor);
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
+        }
+        private void fontFamilyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(fontFamilyComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem);
+            }
+        }
+
+
     }
 }
