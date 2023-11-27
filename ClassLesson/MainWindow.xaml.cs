9906d30a6cc0d03cf6696ad352c2be1d49bedfd9
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-11-27 17:33:42 +0800

    update

--- ClassLesson/MainWindow.xaml.cs
+++ ClassLesson/MainWindow.xaml.cs
@@ -49,6 +49,43 @@ namespace ClassLesson
                 Type = "必修"
             });
 
+            Teacher teacher2 = new Teacher() { TeacherName = "陳福坤" };
+            teacher2.TeachingCourses.Add(new Course(teacher2)
+            {
+                CourseName = "計算機概論",
+                OpeningClass = "四技一丙",
+                Point = 2,
+                Type = "必修"
+            });
+            teacher2.TeachingCourses.Add(new Course(teacher2)
+            {
+                CourseName = "計算機概論",
+                OpeningClass = "四技一甲一乙",
+                Point = 2,
+                Type = "必修"
+            });
+
+            Teacher teacher3 = new Teacher() { TeacherName = "許子衡" };
+            teacher3.TeachingCourses.Add(new Course(teacher3)
+            {
+                CourseName = "Android程式設計",
+                OpeningClass = "四技一甲一乙",
+                Point = 3,
+                Type = "選修"
+            });
+            teacher3.TeachingCourses.Add(new Course(teacher3)
+            {
+                CourseName = "人工智慧與雲端運算",
+                OpeningClass = "五專資工三甲",
+                Point = 3,
+                Type = "必修"
+            });
+
+            teachers.Add(teacher1);
+            teachers.Add(teacher2);
+            teachers.Add(teacher3);
+
+            tvTeacher.ItemsSource = teachers;
         }
 
         private void InitializeStudent()
