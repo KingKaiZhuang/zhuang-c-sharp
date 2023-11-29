6db0d1d987dd6a2e2bdc3b18486f01780562f1fa
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-11-29 11:00:43 +0800

    update

--- ClassLesson/Course.cs
+++ ClassLesson/Course.cs
@@ -4,10 +4,10 @@ namespace ClassLesson
 {
     class Course
     {
-        public String CourseName { get; set; }
-        public String Type {  get; set; }
+        public string CourseName { get; set; }
+        public string Type {  get; set; }
         public int Point {  get; set; }
-        public String OpeningClass {  get; set; }
+        public string OpeningClass {  get; set; }
         Teacher Tutor { get; set; }
         public Course(Teacher tutor)
         {
