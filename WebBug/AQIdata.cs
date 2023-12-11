c7368504acb75521f57d20d9fc653cb95eb8e5c3
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-12-11 15:07:25 +0800

    update

---
+++ WebBug/AQIdata.cs
@@ -0,0 +1,69 @@
+﻿namespace WebBug
+{
+
+    public class AQIdata
+    {
+        public Field[] fields { get; set; }
+        public string resource_id { get; set; }
+        public __Extras __extras { get; set; }
+        public bool include_total { get; set; }
+        public string total { get; set; }
+        public string resource_format { get; set; }
+        public string limit { get; set; }
+        public string offset { get; set; }
+        public _Links _links { get; set; }
+        public Record[] records { get; set; }
+    }
+
+    public class __Extras
+    {
+        public string api_key { get; set; }
+    }
+
+    public class _Links
+    {
+        public string start { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Field
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Info info { get; set; }
+    }
+
+    public class Info
+    {
+        public string label { get; set; }
+    }
+
+    public class Record
+    {
+        public string sitename { get; set; }
+        public string county { get; set; }
+        public string aqi { get; set; }
+        public string pollutant { get; set; }
+        public string status { get; set; }
+        public string so2 { get; set; }
+        public string co { get; set; }
+        public string o3 { get; set; }
+        public string o3_8hr { get; set; }
+        public string pm10 { get; set; }
+        public string pm25 { get; set; }
+        public string no2 { get; set; }
+        public string nox { get; set; }
+        public string no { get; set; }
+        public string wind_speed { get; set; }
+        public string wind_direc { get; set; }
+        public string publishtime { get; set; }
+        public string co_8hr { get; set; }
+        public string pm25_avg { get; set; }
+        public string pm10_avg { get; set; }
+        public string so2_avg { get; set; }
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+        public string siteid { get; set; }
+    }
+
+}
--- WebBug/MainWindow.xaml.cs
+++ WebBug/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WebBug
 {
@@ -20,9 +13,48 @@ namespace WebBug
     /// </summary>
     public partial class MainWindow : Window
     {
+        string url = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=JSON";
+
+        AQIdata aqidata = new AQIdata();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
+
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = url;
+        }
+
+        private async void GetWebDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "正在抓取網路資料...";
+            string jsontext = await FetchContentAsync(url);
+            ContentTextBox.Text = jsontext;
+            aqidata = JsonSerializer.Deserialize<AQIdata>(jsontext);
+            fields = aqidata.fields.ToList();
+            records = aqidata.records.ToList();
+            StatusTextBlock.Text = $"共有 {records.Count} 筆資料";
+            DisplayAQIData();
+        }
+
+        private void DisplayAQIData()
+        {
+            RecordDataGrid.ItemsSource = records;
+        }
+
+        private async Task<string> FetchContentAsync(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"發生錯誤: {ex.Message}";
+            }
         }
     }
 }
