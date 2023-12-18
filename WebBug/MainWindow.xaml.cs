66a9fd1b15356b6beb2af4b0e5e45acc577bb026
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-12-18 14:22:11 +0800

    update

--- WebBug/MainWindow.xaml.cs
+++ WebBug/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using WebBug;
+using System.Linq;
 
 namespace WebBug
 {
@@ -16,27 +17,29 @@ namespace WebBug
     /// </summary>
     public partial class MainWindow : Window
     {
-        string url = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=JSON";
-
+        string url = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
         AQIdata aqidata = new AQIdata();
         List<Field> fields = new List<Field>();
         List<Record> records = new List<Record>();
+        List<Record> selectedRecords = new List<Record>();
         SeriesCollection seriesCollection = new SeriesCollection();
-
         public MainWindow()
         {
             InitializeComponent();
             UrlTextBox.Text = url;
+            selectedRecords.Clear();
         }
 
         private async void GetWebDataButton_Click(object sender, RoutedEventArgs e)
         {
             ContentTextBox.Text = "正在抓取網路資料...";
+
             string jsontext = await FetchContentAsync(url);
             ContentTextBox.Text = jsontext;
             aqidata = JsonSerializer.Deserialize<AQIdata>(jsontext);
             fields = aqidata.fields.ToList();
             records = aqidata.records.ToList();
+            selectedRecords = records;
             StatusTextBlock.Text = $"共有 {records.Count} 筆資料";
             DisplayAQIData();
         }
@@ -46,25 +49,26 @@ namespace WebBug
             RecordDataGrid.ItemsSource = records;
             DataWrapPanel.Children.Clear();
 
-            foreach(var field in fields)
+            foreach (var field in fields)
             {
                 var propertyInfo = typeof(Record).GetProperty(field.id);
-                if(propertyInfo != null)
+                if (propertyInfo != null)
                 {
                     string value = propertyInfo.GetValue(records[0]) as string;
-                    if(double.TryParse(value, out double v))
+                    if (double.TryParse(value, out double v))
                     {
                         CheckBox cb = new CheckBox
                         {
                             Content = field.info.label,
                             Tag = field.id,
-                            Margin = new Thickness(5),
+                            Margin = new Thickness(3),
                             Width = 120,
                             FontSize = 14,
-                            FontWeight = FontWeights.Bold
+                            FontWeight = FontWeights.Bold,
                         };
 
                         cb.Checked += UpdateChart;
+                        cb.Unchecked += UpdateChart;
                         DataWrapPanel.Children.Add(cb);
                     }
                 }
@@ -74,19 +78,20 @@ namespace WebBug
         private void UpdateChart(object sender, RoutedEventArgs e)
         {
             seriesCollection.Clear();
-            foreach(CheckBox cb in DataWrapPanel.Children)
+
+            foreach (CheckBox cb in DataWrapPanel.Children)
             {
                 if (cb.IsChecked == true)
                 {
-                    var tab = cb.Tag as String;
+                    var tag = cb.Tag as String;
                     ColumnSeries columnSeries = new ColumnSeries();
                     ChartValues<double> values = new ChartValues<double>();
                     List<String> labels = new List<String>();
 
-                    foreach(var record in records)
+                    foreach (var record in selectedRecords)
                     {
-                        var propertyInfo = typeof(Record).GetProperty(tab);
-                        if(propertyInfo != null)
+                        var propertyInfo = typeof(Record).GetProperty(tag);
+                        if (propertyInfo != null)
                         {
                             string value = propertyInfo.GetValue(record) as string;
                             if (double.TryParse(value, out double v))
@@ -97,8 +102,8 @@ namespace WebBug
                         }
                     }
                     columnSeries.Values = values;
-                    columnSeries.Title = tab;
-                    columnSeries.LabelPoint = point => $"{labels[(int)point.X]}";
+                    columnSeries.Title = tag;
+                    columnSeries.LabelPoint = point => $"{labels[(int)point.X]}: {point.Y.ToString()}";
                     seriesCollection.Add(columnSeries);
                 }
             }
@@ -118,6 +123,17 @@ namespace WebBug
             {
                 return $"發生錯誤: {ex.Message}";
             }
+        }
+
+        private void RecordDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedRecords = RecordDataGrid.SelectedItems.Cast<Record>().ToList();
+            StatusTextBlock.Text = $"共選取 {selectedRecords.Count} 筆資料";
+        }
+
+        private void RecordDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;
         }
     }
 }
